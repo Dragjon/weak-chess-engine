c81c7293d4e21c351d4430248a3495aabf755423
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-09 18:47:52 +0800

    NMR Refactor. Bench: 572431

--- src/search.cpp
+++ src/search.cpp
@@ -204,9 +204,10 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
     // except if it's in a zugzwang. Hence, if we skip out turn and
     // we still maintain beta, then we can prune early. Also do not
     // do NMP when tt suggests that it should fail immediately
-    if (!pv_node && !node_is_check && static_eval >= beta + 184 - 22 * depth && depth >= null_move_depth.current && (!tt_hit || !(entry.type == NodeType::UPPERBOUND) || entry.score >= beta) && (board.hasNonPawnMaterial(Color::WHITE) || board.hasNonPawnMaterial(Color::BLACK))){
+    if (!pv_node && !node_is_check && static_eval >= beta && depth >= null_move_depth.current && (!tt_hit || !(entry.type == NodeType::UPPERBOUND) || entry.score >= beta) && (board.hasNonPawnMaterial(Color::WHITE) || board.hasNonPawnMaterial(Color::BLACK))){
         board.makeNullMove();
-        int32_t score = -alpha_beta(board, depth - null_move_reduction.current, -beta, -beta+1, ply + 1);
+        int32_t reduction = 3 + depth / 3;
+        int32_t score = -alpha_beta(board, depth - reduction, -beta, -beta+1, ply + 1);
         board.unmakeNullMove();
 
         if (score >= beta)
