070c0d108f3c8cf1adf639ba1cb014e8a0aea7dc
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-27 15:45:03 +0800

    Bench: 653269

--- src/search.cpp
+++ src/search.cpp
@@ -413,11 +413,13 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             else if (singular_beta >= beta)
                 return singular_beta;
 
-            
             // Negative extensions
             // Potential for multi-cut
             else if (entry.score >= beta)
                 extension = -3;
+
+            else if (entry.score <= alpha)
+                extension = -1;
         }
 
         // Static Exchange Evaluation Pruning
