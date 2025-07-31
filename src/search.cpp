b18dba6694162a33bd246df4757a4c9c3497cb1a
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-31 19:12:12 +0800

    Bench: 677706

--- src/search.cpp
+++ src/search.cpp
@@ -409,9 +409,9 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
                     extension = 2;
             }
 
-            // Multi-cut pruning
-            else if (singular_beta >= beta)
-                return singular_beta;
+            // Multi-cut pruning (fail-soft version)
+            else if (score >= beta)
+                return score;
             
             // Negative extensions
             // Potential for multi-cut
