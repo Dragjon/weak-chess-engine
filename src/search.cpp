5c51c1b9dd23f51bc387d746ad498d840ba803f3
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-06 01:04:08 +0800

    Bench: 786437

--- src/search.cpp
+++ src/search.cpp
@@ -449,7 +449,7 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             reduction -= abs(raw_eval - static_eval) > late_move_reduction_corrplexity.current;
 
             // Reduce less when in check
-            reduction += in_check;
+            reduction -= in_check;
         }
 
         int32_t score = 0;
