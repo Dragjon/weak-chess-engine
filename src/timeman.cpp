329938f1ff0ea28ce724c16809718e82b8a20478
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-05 16:27:20 +0800

    Bench: 879580

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -5,4 +5,4 @@
 std::chrono::time_point<std::chrono::system_clock> search_start_time = std::chrono::system_clock::now();
 int64_t max_soft_time_ms = 10000ll;
 int64_t max_hard_time_ms = 30000ll;
-int64_t move_overhead_ms = 0; 
+int64_t move_overhead_ms = 0;  
