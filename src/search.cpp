4eecc71a68f52d9acb076221bb39bd0c2150f2e7
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-06 21:22:25 +0800

    Min lmr reduction. Bench: 822653

--- src/search.cpp
+++ src/search.cpp
@@ -485,6 +485,9 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             // Similar concept to futility pruning but we can be more aggressive
             // STC: 8.66 +- 5.65
             reduction += static_eval + 50 + 50 * depth <= alpha;
+
+            // Min LMR is set as 0
+            reduction = max(0, reduction);
         }
 
         int32_t score = 0;
