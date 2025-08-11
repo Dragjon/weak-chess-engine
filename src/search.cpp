e6931d9b8bfb5096f748333c97807ffd0c31bf5e
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-11 23:44:18 +0800

    Bench: 607246

--- src/search.cpp
+++ src/search.cpp
@@ -505,6 +505,9 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             // Reduce more in cut nodes
             // STC: 6.47 +- 4.60
             reduction += cut_node;
+
+            // Reduce less in pv nodes
+            reduction -= pv_node;
         }
 
         // Capture late move reductions - since the move is a capture
