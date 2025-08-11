67b3d57d04909e3f8a95e9a996beacb78ae45e94
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-11 12:13:59 +0800

    Bench: 577387

--- src/defaults.cpp
+++ src/defaults.cpp
@@ -5,98 +5,102 @@ SearchParam tt_size("Hash", 64, 1, 16384, 1);
 SearchParam threads("Threads", 1, 1, 1, 1);
 SearchParam move_overhead("MoveOverhead", 0, 0, 10000, 1);
 
-// SPSA (https://chess.n9x.co/tune/3573/)
+// SPSA (https://kelseyde.pythonanywhere.com/tune/969/)
 /*
-ReverseFutilityMargin, 49
+ReverseFutilityMargin, 46
 NullMoveBase, 4
-NullMoveDivisor, 4
-LateMoveReductionBase, 68
-LateMoveReductionMultiplier, 50
-LateMoveReductionCorrplexity, 87
-LateMovePruningBase, 3
-LateMovePruningQuad, 3
-FutilityEvalBase, 104
-FutilityDepthMul, 101
-AspirationWindowDelta, 9
-AspirationWideningFactor, 3
-SeeNoisyMargin, -98
-SeeQuietMargin, -55
-QuietHistoryPruningQuad, 2004
-HistoryBonusBase, 94
-HistoryBonusMulLinear, 219
-HistoryBonusMulQuad, 417
-HistoryMalusBase, 1
-HistoryMalusMulLinear, 260
-HistoryMalusMulQuad, 145
-HistoryReductionDepthMul, 992
-RazoringBase, 616
-RazoringQuadMul, 308
-SEEPawn, 105
-SEEKnight, 340
-SEEBishop, 312
-SEERook, 502
-SEEQueen, 928
-DeltaPruningPawnBonus, 134
-Tempo, 4
-SoftTMRatio, 29
-HardTMRatio, 1
-NodeTMBase, 211
-NodeTMMul, 113
+NullMoveDivisor, 3
+LateMoveReductionBase, 89
+LateMoveReductionMultiplier, 48
+LateMoveReductionCorrplexity, 98
+LateMovePruningBase, 4
+LateMovePruningQuad, 1
+FutilityEvalBase, 97
+FutilityDepthMul, 90
+AspirationWindowDelta, 11
+AspirationWideningFactor, 18
+SeeNoisyMargin, -101
+SeeQuietMargin, -45
+QuietHistoryPruningQuad, 2028
+HistoryBonusBase, -51
+HistoryBonusMulLinear, 205
+HistoryBonusMulQuad, 411
+HistoryMalusBase, 56
+HistoryMalusMulLinear, 285
+HistoryMalusMulQuad, 201
+HistoryReductionDiv, 7654
+RazoringBase, 630
+RazoringQuadMul, 305
+SEEPawn, 99
+SEEKnight, 329
+SEEBishop, 296
+SEERook, 510
+SEEQueen, 955
+DeltaPruningPawnBonus, 140
+Tempo, 2
+SoftTMRatio, 27
+HardTMRatio, 2
+NodeTMBase, 223
+NodeTMMul, 112
+LMRFutilityBase, 56
+LMRFutilityMultiplier, 48
+CaptLMRBase, 25
+CaptLMRMultiplier, 27
 */
 
-SearchParam reverse_futility_margin("ReverseFutilityMargin", 49, 30, 100, 10);
+SearchParam reverse_futility_margin("ReverseFutilityMargin", 46, 30, 100, 10);
 
 SearchParam null_move_base("NullMoveBase", 4, 1, 6, 1);
-SearchParam null_move_divisor("NullMoveDivisor", 4, 1, 8, 1);
+SearchParam null_move_divisor("NullMoveDivisor", 3, 1, 8, 1);
 
-SearchParam late_move_reduction_base("LateMoveReductionBase", 68, 30, 120, 15);
-SearchParam late_move_reduction_multiplier("LateMoveReductionMultiplier", 50, 10, 70, 7);
-SearchParam late_move_reduction_corrplexity("LateMoveReductionCorrplexity", 87, 50, 150, 7);
+SearchParam late_move_reduction_base("LateMoveReductionBase", 89, 30, 120, 15);
+SearchParam late_move_reduction_multiplier("LateMoveReductionMultiplier", 48, 10, 70, 7);
+SearchParam late_move_reduction_corrplexity("LateMoveReductionCorrplexity", 98, 50, 150, 7);
 
-SearchParam late_move_pruning_base("LateMovePruningBase", 3, 1, 10, 1);
-SearchParam late_move_pruning_quad("LateMovePruningQuad", 3, 1, 10, 1);
+SearchParam late_move_pruning_base("LateMovePruningBase", 4, 1, 10, 1);
+SearchParam late_move_pruning_quad("LateMovePruningQuad", 1, 1, 10, 1);
 
-SearchParam futility_eval_base("FutilityEvalBase", 104, 50, 200, 15);
-SearchParam futility_depth_mul("FutilityDepthMul", 101, 50, 200, 12);
+SearchParam futility_eval_base("FutilityEvalBase", 97, 50, 200, 15);
+SearchParam futility_depth_mul("FutilityDepthMul", 90, 50, 200, 12);
 
-SearchParam aspiration_window_delta("AspirationWindowDelta", 9, 5, 30, 2);
-SearchParam aspiration_widening_factor("AspirationWideningFactor", 25, 1, 200, 10);
+SearchParam aspiration_window_delta("AspirationWindowDelta", 11, 5, 30, 2);
+SearchParam aspiration_widening_factor("AspirationWideningFactor", 18, 1, 200, 10);
 
-SearchParam see_noisy_margin("SeeNoisyMargin", -98, -120, -30, 6);
-SearchParam see_quiet_margin("SeeQuietMargin", -55, -120, -30, 6);
+SearchParam see_noisy_margin("SeeNoisyMargin", -101, -120, -30, 6);
+SearchParam see_quiet_margin("SeeQuietMargin", -45, -120, -30, 6);
 
-SearchParam quiet_history_pruning_quad("QuietHistoryPruningQuad", 2004, 786, 8192, 64);
+SearchParam quiet_history_pruning_quad("QuietHistoryPruningQuad", 2028, 786, 8192, 64);
 
-SearchParam history_bonus_base("HistoryBonusBase", 94, -384, 768, 64);
-SearchParam history_bonus_mul_linear("HistoryBonusMulLinear", 219, 64, 384, 32);
-SearchParam history_bonus_mul_quad("HistoryBonusMulQuad", 417, 1, 1536, 64);
+SearchParam history_bonus_base("HistoryBonusBase", -51, -384, 768, 64);
+SearchParam history_bonus_mul_linear("HistoryBonusMulLinear", 205, 64, 384, 32);
+SearchParam history_bonus_mul_quad("HistoryBonusMulQuad", 411, 1, 1536, 64);
 
-SearchParam history_malus_base("HistoryMalusBase", 1, -384, 768, 64);
-SearchParam history_malus_mul_linear("HistoryMalusMulLinear", 260, 64, 384, 32);
-SearchParam history_malus_mul_quad("HistoryMalusMulQuad", 145, 1, 1536, 64);
+SearchParam history_malus_base("HistoryMalusBase", 56, -384, 768, 64);
+SearchParam history_malus_mul_linear("HistoryMalusMulLinear", 285, 64, 384, 32);
+SearchParam history_malus_mul_quad("HistoryMalusMulQuad", 201, 1, 1536, 64);
 
-SearchParam history_reduction_div("HistoryReductionDiv", 8192, 1024, 16384, 512);
+SearchParam history_reduction_div("HistoryReductionDiv", 7654, 1024, 16384, 512);
 
-SearchParam razoring_base("RazoringBase", 616, -384, 768, 15);
-SearchParam razoring_quad_mul("RazoringQuadMul", 308, 1, 1536, 15);
+SearchParam razoring_base("RazoringBase", 630, -384, 768, 15);
+SearchParam razoring_quad_mul("RazoringQuadMul", 305, 1, 1536, 15);
 
-SearchParam see_pawn("SEEPawn", 105, 70, 130, 15);
-SearchParam see_knight("SEEKnight", 340, 250, 450, 15);
-SearchParam see_bishop("SEEBishop", 312, 250, 450, 15);
-SearchParam see_rook("SEERook", 502, 400, 650, 15);
-SearchParam see_queen("SEEQueen", 928, 800, 1200, 15);
+SearchParam see_pawn("SEEPawn", 99, 70, 130, 15);
+SearchParam see_knight("SEEKnight", 329, 250, 450, 15);
+SearchParam see_bishop("SEEBishop", 296, 250, 450, 15);
+SearchParam see_rook("SEERook", 510, 400, 650, 15);
+SearchParam see_queen("SEEQueen", 955, 800, 1200, 15);
 
-SearchParam delta_pruning_pawn_bonus("DeltaPruningPawnBonus", 134, 80, 200, 20);
+SearchParam delta_pruning_pawn_bonus("DeltaPruningPawnBonus", 140, 80, 200, 20);
 
-SearchParam tempo("Tempo", 4, 0, 20, 2);
+SearchParam tempo("Tempo", 2, 0, 20, 2);
 
-SearchParam soft_tm_ratio("SoftTMRatio", 29, 5, 50, 8);
-SearchParam hard_tm_ratio("HardTMRatio", 1, 1, 20, 4);
-SearchParam node_tm_base("NodeTMBase", 211, 50, 300, 20);
-SearchParam node_tm_mul("NodeTMMul", 113, 50, 300, 20);
+SearchParam soft_tm_ratio("SoftTMRatio", 27, 5, 50, 8);
+SearchParam hard_tm_ratio("HardTMRatio", 2, 1, 20, 4);
+SearchParam node_tm_base("NodeTMBase", 223, 50, 300, 20);
+SearchParam node_tm_mul("NodeTMMul", 112, 50, 300, 20);
 
-SearchParam lmr_futility_base("LMRFutilityBase", 50, 20, 80, 15);
-SearchParam lmr_futility_multiplier("LMRFutilityMultiplier", 50, 20, 80, 7);
+SearchParam lmr_futility_base("LMRFutilityBase", 56, 20, 80, 15);
+SearchParam lmr_futility_multiplier("LMRFutilityMultiplier", 48, 20, 80, 7);
 
-SearchParam capt_lmr_base("CaptLMRBase", 38, 15, 50, 15);
-SearchParam capt_lmr_multiplier("CaptLMRMultiplier", 26, 10, 40, 7);
+SearchParam capt_lmr_base("CaptLMRBase", 25, 15, 50, 15);
+SearchParam capt_lmr_multiplier("CaptLMRMultiplier", 27, 10, 40, 7);
