5a9834c192447481665801519303e3c93f3eb53a
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-11 23:30:13 +0800

    Bench: 684916

--- src/search.cpp
+++ src/search.cpp
@@ -503,6 +503,7 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             reduction -= tt_was_pv;
 
             // Reduce more in cut nodes
+            // STC: 6.47 +- 4.60
             reduction += cut_node;
         }
 
