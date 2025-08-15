16832870e131bcdf6a378d56bd3256b6f385c29a
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-16 00:53:50 +0800

    Bench: 542882

--- src/search.cpp
+++ src/search.cpp
@@ -511,6 +511,9 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             // Reduce less for killer moves
             // STC: 5.45 +- 4.10
             reduction -= (killers[0][ply] == current_move) || (killers[1][ply] == current_move);
+
+            // Reduce more for noisy transposition
+            reduction += tt_hit && board.isCapture(entry.best_move);
         }
 
         // Capture late move reductions - since the move is a capture
