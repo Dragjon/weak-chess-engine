9234a5995046b3e6716cb3a8e4cefb1398b039f0
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-08-19 18:30:36 +0800

    Bench: 695925

--- src/search.cpp
+++ src/search.cpp
@@ -286,7 +286,7 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
     // move history. We reduce the margin if the parent move is bad
     // an increase margin if parent move is good.
     // STC: 132.85 +/- 26.94
-    int32_t rfp_margin = reverse_futility_margin.current * depth + search_info.parent_hist_score / 500;
+    int32_t rfp_margin = reverse_futility_margin.current * depth + search_info.parent_hist_score / 600;
     if (!pv_node 
         && !tt_was_pv 
         && !in_check 
